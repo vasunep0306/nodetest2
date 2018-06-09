b72209652df3fd2682fd9effe55d56378f08affe
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-06-09 09:54:38 -0400

    add ajax

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -8,3 +8,31 @@ $(document).ready(function() {
 });
 
 // Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+  // Empty content string
+  var tableContent = "";
+  // jQuery AJAX call for JSON
+  $.getJSON("/users/userList", function(data) {
+    // For each item in our JSON, add a table row and cells to the content string
+    $.each(data, function() {
+      tableContent += "<tr>";
+      tableContent +=
+        '<td><a href="#" class="linkshowuser" rel="' +
+        this.username +
+        '">' +
+        this.username +
+        "</a></td>";
+      tableContent += "<td>" + this.email + "</td>";
+      tableContent +=
+        '<td><a href="#" class="linkdeleteuser" rel="' +
+        this._id +
+        '">delete</a></td>';
+      tableContent += "</tr>";
+    });
+
+    // Inject the whole content string into our existing HTML table
+    $("#userList table tbody").html(tableContent);
+  });
+}
