5d227558cfc5acdb1e6e1bc308a7837e919153dd
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-06-09 09:49:39 -0400

    Let's start by defining a function to populates our HTML table with data

---
+++ public/javascripts/global.js
@@ -0,0 +1,10 @@
+// Userlist data array for filling in info box
+var userListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+  // Populate the user table on initial page load
+  populateTable();
+});
+
+// Functions =============================================================
